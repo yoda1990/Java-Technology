332de26bdf0a891973d252052b027be92255b357
You-Hmily <706097141@qq.com>
2020-07-23 23:25:05 +0800

    长连接网关开发

---
+++ io-technology/keep-alive-gateway/src/main/java/org/hmily/keepalive/gateway/KeepAliveGatewayApplication.java
@@ -0,0 +1,15 @@
+package org.hmily.keepalive.gateway;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepAliveGatewayApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeepAliveGatewayApplication.class,args);
+    }
+
+}
