776f41e2d41eda08d53a6ecacaacb5129e42cdfb
You-Hmily <706097141@qq.com>
2020-07-27 23:17:03 +0800

    添加核心模块

---
+++ io-technology/keep-alive-gateway/keep-alive-core/src/main/java/org/hmily/keepalive/core/Topic.java
@@ -0,0 +1,21 @@
+package org.hmily.keepalive.core;
+
+
+/**
+ *  MQTT 协议 Topic
+ */
+
+public class Topic {
+
+
+    private String topic;
+
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+}
